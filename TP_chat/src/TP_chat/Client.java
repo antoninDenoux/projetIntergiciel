4e50215084482ceb64721793e7cdbac81a59042c
antonin.denoux <antonin.denoux@gmail.com>
2017-10-13 15:57:11 +0200

    Changed Client class to be able to write the commands in the console.

--- TP_chat/src/TP_chat/Client.java
+++ TP_chat/src/TP_chat/Client.java
@@ -1,5 +1,8 @@
 package TP_chat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
@@ -8,18 +11,37 @@ import java.rmi.RemoteException;
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Dialogue myComponent;
 		try {
+			boolean boucle = true;
 			myComponent = (Dialogue) Naming.lookup("Dialogue");
-			System.out.println(myComponent.connect("Bob"));
-			System.out.println(myComponent.connect("JJG"));
-			System.out.println(myComponent.connect("Bob"));
-			System.out.println(myComponent.sendMessage("Toto", "Bob", "Premier message"));
-			System.out.println(myComponent.sendMessage("Toto", "Bob", "Second message"));
-			System.out.println(myComponent.getMessages("Bob"));
-			System.out.println(myComponent.getClients());
-		} catch (MalformedURLException | RemoteException | NotBoundException e) {
+			BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Bienvenue sur le tchat, taper help pour l'aide");
+			while (boucle) {
+				String theLine = userIn.readLine();
+				String[] req = theLine.split(" ");
+				switch (req[0]) {
+				case ("help"):
+					System.out.println("Commandes disponibles: connect pseudo ; end ; sendMessage");
+					break;
+				case ("connect"):
+					System.out.println(myComponent.connect(req[1]));
+					break;
+				case ("sendMessage"):
+					System.out.println(myComponent.sendMessage(req[1], req[2], req[3]));
+					break;
+				case ("getMessages"):
+					System.out.println(myComponent.getMessages(req[1]));
+					break;
+				case ("end"):
+					boucle = false;
+					System.out.println("Fin du tchat");
+					break;
+				default:
+					System.out.println("Commande inconnue");
+				}
+			}
+		} catch (NotBoundException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
